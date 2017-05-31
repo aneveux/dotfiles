b972b5d10df191750f4e1afdb8992dfaa5f75fb6
Antoine Neveux <antoine.neveux@gmail.com>
2017-05-31 13:41:12 +0200

    Fixed lssh completion!

--- fish/completions/lssh.fish
+++ fish/completions/lssh.fish
@@ -1,11 +1,6 @@
 function __fish_complete_lssh
-	set -l parts (string split : $argv[1])
-	switch (count $parts) 
-		case '2';
-			set -l path (string split -r -m1 / $parts[2])
-			ssh $parts[1] "ls -l $path"
-		case '*';
-	end
+	set -l token (commandline -t)
+	fish -c "complete -C\"scp $token\""
 end
 
 complete -x -c lssh -a '(__fish_complete_lssh)'
