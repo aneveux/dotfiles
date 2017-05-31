f09edc75b643d0f21a925b14fa1c6c54139504fc
Antoine Neveux <antoine.neveux@gmail.com>
2017-05-31 13:41:28 +0200

    Better project completion thanks to @jmdesprez!

--- fish/completions/project.fish
+++ fish/completions/project.fish
@@ -1,5 +1,33 @@
-function __fish_complete_project
-  find ~/git/ \( -name 'build.gradle' -o -name 'pom.xml' \) -type f -printf '%P\n' | xargs -n1 dirname | awk '{print $0 "\tIJ"}'
+function __complete_project --description "build autocompletion for project command"
+  set -l last_char (string sub -s -1 (commandline -p))
+  set -l cmd_line (commandline -po)
+  if test (count $cmd_line) -eq 2 -a "$last_char" != " "
+    find ~/git/ -name 'pom.xml' -type f -printf '%P\n' | xargs -n1 dirname | awk '{print $0 "\tMVN"}'
+    find ~/git/ -name 'build.gradle' -type f -printf '%P\n' | xargs -n1 dirname | awk '{print $0 "\tGR"}'
+  else
+    set -l project $cmd_line[2]
+    if test (count $cmd_line) -eq 2
+      if test -f $HOME/git/$project/pom.xml
+        echo "mvn"
+      end
+      if test -f $HOME/git/$project/build.gradle
+        if test -f $HOME/git/$project/gradlew
+          echo -e "./gradlew\tWrapper"
+          echo -e "$HOME/git/$project/gradlew\tWrapper"
+        end
+        echo "gradle"
+      end
+      if test -d $HOME/git/$project/.git
+        echo "git"
+      end
+    else
+      if test "$last_char" = " "
+        fish -c "cd $HOME/git/$project; and complete -C\"$cmd_line[3..-1] \""
+      else
+        fish -c "cd $HOME/git/$project; and complete -C\"$cmd_line[3..-1]\""
+      end
+    end
+  end
 end
 
-complete -x -c project -a '(__fish_complete_project)'
+complete -c project -x -a '(__complete_project)'
