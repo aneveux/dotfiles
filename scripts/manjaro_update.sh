630f96da0d350a050666c0a6208e7a769b365ee9
Antoine Neveux <antoine.neveux@worldline.com>
2017-06-08 17:59:59 +0200

    Updated manjaro update script

--- scripts/manjaro_update.sh
+++ scripts/manjaro_update.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
-sudo pacman-mirrors -g -c France
-sudo test -f /var/lib/pacman/db.lck && sudo rm /var/lib/pacman/db.lck
-sudo pacman -Syu
+#!/bin/bash
+test -f /var/lib/pacman/db.lck && sudo rm /var/lib/pacman/db.lck
+sudo -E pacman-mirrors -g -c France
+sudo -E pacman -Syu
