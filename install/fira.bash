bda008b0257c6701461036fb555ad01dba5a554c
Antoine Neveux <aneveux@protonmail.com>
2017-07-19 00:54:02 +0200

    Refresh the font cache automatically after installing Fira

--- install/fira.bash
+++ install/fira.bash
@@ -3,7 +3,9 @@ function install_fira {
 	for type in Bold Light Medium Regular Retina; do
 	    wget -O ~/.fonts/FiraCode-${type}.ttf \
 	    "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
-	done	
+	done
+
+	fc-cache -f
 }
 
 test -e ~/.fonts/FiraCode-Regular.ttf || install_fira
