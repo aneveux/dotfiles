3f204eb56846d8192df88601806f6257d4dd9893
Antoine Neveux <aneveux@protonmail.com>
2017-09-16 23:43:14 +0200

    Added env variables for SDKMAN binaries

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,12 @@ set -xg GPG_TTY (tty)
 # sdkman
 set -xg PATH $PATH (find $HOME/.sdkman/candidates/*/current/bin -maxdepth 0)
 
+set -xg JAVA_HOME $HOME/.sdkman/candidates/java/current/
+set -xg MAVEN_HOME $HOME/.sdkman/candidates/maven/current/
+set -xg GRADLE_HOME $HOME/.sdkman/candidates/gradle/current/
+set -xg KOTLIN_HOME $HOME/.sdkman/candidates/kotlin/current/
+set -xg SCALA_HOME $HOME/.sdkman/candidates/scala/current/
+
 begin
     set --local AUTOJUMP_PATH /usr/share/autojump/autojump.fish
     if test -e "$AUTOJUMP_PATH"
