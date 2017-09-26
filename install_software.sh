eb164105763779d11544932a6d54de57d88992ff
Antoine Neveux <antoine.neveux@worldline.com>
2017-09-26 17:14:54 +0200

    Adding pgadmin 4 installation + shortcuts

--- install_software.sh
+++ install_software.sh
@@ -14,7 +14,7 @@ chsh -s /usr/bin/fish
 sudo -E pacman -S --needed acpi cowsay dropbox dunst feh filezilla firefox gnupg gpa htop httpie keepassxc lolcat nitrogen playerctl ranger sysstat thunderbird tmux tree w3m workrave xclip
 
 # Tooling
-sudo -E pacman -S --needed ansible corkscrew docker docker-compose docker-machine git jq pgadmin3 qutebrowser ruby ruby-docs
+sudo -E pacman -S --needed ansible corkscrew docker docker-compose docker-machine git jq pgadmin4 qutebrowser ruby ruby-docs
 
 # Editors
 sudo -E pacman -S --needed atom cherrytree gvim vim 
