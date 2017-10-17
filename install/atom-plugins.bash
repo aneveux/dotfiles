19fbea8c71e9f0a5d566c6278a970f9928c961b0
Antoine Neveux <aneveux@protonmail.com>
2017-10-18 00:13:17 +0200

    Adding automated Atom plugins installation

---
+++ install/atom-plugins.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+plugins=(atom-beautify atom-jinja2 autoclose-html busy-signal clipboard-plus color-picker docblockr emmet file-icons fonts git-plus highlight-selected intentions isotope-ui language-ansible language-fish-shell linter linter-ui-default merge-conflicts monokai pigments project-manager todo-show)
+for plugin in "${plugins[@]}"
+do
+	if !test -f $HOME/.atom/packages/$plugin; then
+		apm install $plugin
+	fi
+done
