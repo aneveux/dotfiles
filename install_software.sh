ec5f4ab02a7bc5c80b77babd54234e2ff175a489
Antoine Neveux <aneveux@protonmail.com>
2017-10-18 01:10:07 +0200

    Enhanced my gitconfig a lot :)

--- install_software.sh
+++ install_software.sh
@@ -11,13 +11,13 @@ sudo -E pacman -S --needed zsh zsh-completions fish thefuck terminator termite
 chsh -s /usr/bin/fish
 
 # Utils
-sudo -E pacman -S --needed acpi cowsay dmenu dropbox dunst feh filezilla firefox gnupg gpa htop httpie keepassxc lolcat nitrogen pacli playerctl ranger sysstat termite thunderbird tmux tree w3m workrave xclip
+sudo -E pacman -S --needed acpi cowsay diff-so-fancy dmenu dropbox dunst feh filezilla firefox gnupg gpa htop httpie keepassxc lolcat nitrogen pacli playerctl ranger sysstat termite thunderbird tmux tree w3m workrave xclip
 
 # Tooling
 sudo -E pacman -S --needed ansible corkscrew docker docker-compose docker-machine git jq pgadmin4 python qutebrowser ruby ruby-docs
 
 # Editors
-sudo -E pacman -S --needed atom cherrytree gvim vim 
+sudo -E pacman -S --needed atom cherrytree gvim vim
 
 # Themes
 sudo -E pacman -S --needed numix-manjaro-themes numix-reborn-icon-themes
