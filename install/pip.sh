ac632d3eb8dcbc545414e4b424e80e76157db514
Antoine Neveux <aneveux@protonmail.com>
2017-10-17 23:39:14 +0200

    Better way to handle pip installation

--- install/pip.sh
+++ install/pip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if !type pip > /dev/null 2> /dev/null ; then
-	curl -O https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py && rm get-pip.py
+	curl -s https://bootstrap.pypa.io/get-pip.py | python
 fi
