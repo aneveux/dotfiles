0b1317ebc644735135210fb94e537e4703458871
Antoine Neveux <aneveux@protonmail.com>
2017-10-18 00:26:47 +0200

    Adding my atom configuration

--- install/atom-plugins.bash
+++ install/atom-plugins.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-plugins=(atom-beautify atom-jinja2 autoclose-html busy-signal clipboard-plus color-picker docblockr emmet file-icons fonts git-plus highlight-selected intentions isotope-ui language-ansible language-fish-shell linter linter-ui-default merge-conflicts monokai pigments project-manager todo-show)
+plugins=(atom-beautify atom-jinja2 autoclose-html busy-signal clipboard-plus color-picker docblockr emmet file-icons fonts git-plus highlight-selected intentions isotope-ui language-ansible language-fish-shell linter linter-ui-default merge-conflicts monokai pigments project-manager ssh-config todo-show)
 for plugin in "${plugins[@]}"
 do
 	if !test -f $HOME/.atom/packages/$plugin; then
