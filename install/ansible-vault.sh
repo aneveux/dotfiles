225995b31894e8c9d533c0273f6bddc4cb97290a
Antoine Neveux <aneveux@protonmail.com>
2017-10-18 00:13:59 +0200

    Removing pip & ansible-vault from automated installation

--- install/ansible-vault.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if !type ansible-vault > /dev/null 2> /dev/null ; then
-	pip install ansible-vault
-fi
--- install/pip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if !type pip > /dev/null 2> /dev/null ; then
-	curl -s https://bootstrap.pypa.io/get-pip.py | python
-fi
