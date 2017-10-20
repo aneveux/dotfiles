eaaf8d300f97130780c41168199ad0196cdcbb00
Antoine Neveux <antoine.neveux@worldline.com>
2017-10-20 09:58:55 +0200

    Fixed few tests in bash scripts

--- install/atom-plugins.bash
+++ install/atom-plugins.bash
@@ -2,7 +2,7 @@
 plugins=(atom-beautify atom-jinja2 autoclose-html busy-signal clipboard-plus color-picker docblockr emmet file-icons fonts git-plus highlight-selected intentions isotope-ui language-ansible language-fish-shell linter linter-ui-default merge-conflicts monokai pigments project-manager ssh-config todo-show)
 for plugin in "${plugins[@]}"
 do
-	if !test -f $HOME/.atom/packages/$plugin; then
+	if ! test -f $HOME/.atom/packages/$plugin; then
 		apm install $plugin
 	fi
 done
