f77342e626e2bab426d0585331d3fcbca9f804fc
Antoine Neveux <aneveux@protonmail.com>
2017-11-30 00:24:04 +0100

    forcing colors in fish to have the same theme everywhere

--- fish/omf/init.fish
+++ fish/omf/init.fish
@@ -8,6 +8,44 @@
 
 fish_default_key_bindings
 
+# Color scheme Management
+#
+
+# Regular syntax highlighting colors
+set -U fish_color_normal f2f2f2
+set -U fish_color_command f2f2f2
+set -U fish_color_param fdf215
+set -U fish_color_redirection f2f2f2
+set -U fish_color_comment f02093
+set -U fish_color_error f02093 --bold
+set -U fish_color_escape abd543
+set -U fish_color_operator abd543
+set -U fish_color_quote a23095
+set -U fish_color_autosuggestion 505050
+set -U fish_color_valid_path --underline
+
+set -U fish_color_cwd abd543
+set -U fish_color_cwd_root a23095
+
+# Background color for matching quotes and parenthesis
+set -U fish_color_match 03adf1
+
+# Background color for search matches
+set -U fish_color_search_match --background=a23095
+
+# Pager colors
+set -U fish_pager_color_prefix 03adf1
+set -U fish_pager_color_completion f2f2f2
+set -U fish_pager_color_description fdf215
+set -U fish_pager_color_progress 03adf1
+
+#
+# Directory history colors
+#
+
+set -U fish_color_history_current 03adf1
+
+
 # Load some local functions and variables
 
 load-local
